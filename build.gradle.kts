3414fd3b8f0a8bf1569d19df9191ad588ed6b0f1
DjSergio88 <sergiolachie@gmail.com>
2026-05-20 00:50:12 +0200

    fix: Define global repositories for all projects and subprojects to fix lint resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,21 @@ buildscript {
 }
 
 allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            dependencySubstitution {
+                substitute(module("com.github.inorichi.injekt:injekt-core:65b0440")).using(module("uy.kohesive.injekt:injekt-core:1.16.1"))
+                substitute(module("com.github.inorichi.injekt:injekt-core")).using(module("uy.kohesive.injekt:injekt-core:1.16.1"))
+            }
+        }
+    }
+
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions {
             jvmTarget = JavaVersion.VERSION_1_8.toString()
